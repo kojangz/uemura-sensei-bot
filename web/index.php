b1db0131abe7499c27ce889e1e8325dc6d2201a4
Koichi Uemura <koichi400@gmail.com>
2017-10-12 20:35:04 +0700

    TEST2

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>こちらはLINE Messaging APIのデモサイトです。</title>
+<title>上村先生BOT</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-こちらはLINE Messaging APIのデモサイトです。
+上村先生BOT
 </body>
 </html>
